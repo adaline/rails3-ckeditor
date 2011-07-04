50e7baad5867fee96237a1042bb934f7cef9892a
Val <val666@gmail.com>
2011-07-05 00:24:54 +0100

    Added partition support for users, users only see their own files

--- app/controllers/ckeditor_controller.rb
+++ app/controllers/ckeditor_controller.rb
@@ -2,57 +2,65 @@ class CkeditorController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:create]
   before_filter :swf_options, :only => [:images, :files, :create]
   layout "ckeditor"
-  
+
   # GET /ckeditor/images
   def images
-    @images = Ckeditor.image_model.find(:all, :order=>"id DESC")
-    
+    if respond_to?(:current_user) and Ckeditor.file_manager_partition_users
+      @images = Ckeditor.image_model.where(:user_id => current_user).order('id DESC')
+    else
+      @images = Ckeditor.image_model.find(:all, :order=>"id DESC")
+    end
+
     respond_to do |format|
       format.html {}
       format.xml { render :xml=>@images }
     end
   end
-  
+
   # GET /ckeditor/files
   def files
-    @files = Ckeditor.file_model.find(:all, :order=>"id DESC")
-    
+    if respond_to?(:current_user) and Ckeditor.file_manager_partition_users
+      @files = Ckeditor.file_model.where(:user_id => current_user).order("id DESC")
+    else
+      @files = Ckeditor.file_model.find(:all, :order=>"id DESC")
+    end
+
     respond_to do |format|
       format.html {}
       format.xml { render :xml=>@files }
     end
   end
-  
+
   # POST /ckeditor/create/:kind
   def create
     @kind = params[:kind] || 'file'
-    
-    unless params[:CKEditor].blank?	  
+
+    unless params[:CKEditor].blank?
 	    params[@swf_file_post_name] = params.delete(:upload)
 	  end
-	  
+
     klass = case @kind.downcase
       when 'file'  then Ckeditor.file_model
 			when 'image' then Ckeditor.image_model
 	  end
-  
+
 	  @record = klass.new
-	  
+
 	  options = {}
-	  
+
 	  params.each do |k, v|
 	    key = k.to_s.downcase
 	    options[key] = v if @record.respond_to?("#{key}=")
 	  end
-    
+
     @record.attributes = options
     @record.user ||= current_user if respond_to?(:current_user)
-    
+
     if @record.valid? && @record.save
       @text = params[:CKEditor].blank? ? @record.to_json(:only=>[:id, :type], :methods=>[:url, :content_type, :size, :filename, :format_created_at], :root => "asset") : %Q"<script type='text/javascript'>
         window.parent.CKEDITOR.tools.callFunction(#{params[:CKEditorFuncNum]}, '#{Ckeditor::Utils.escape_single_quotes(@record.url_content)}');
       </script>"
-      
+
       render :text => @text
     else
       render :nothing => true
@@ -60,10 +68,10 @@ class CkeditorController < ApplicationController
   end
 
   private
-    
+
     def swf_options
       @swf_file_post_name = Ckeditor.swf_file_post_name
-      
+
       if params[:action] == 'images'
         @file_size_limit        = Ckeditor.swf_image_file_size_limit
 			  @file_types             = Ckeditor.swf_image_file_types
@@ -75,7 +83,7 @@ class CkeditorController < ApplicationController
 		    @file_types_description = Ckeditor.swf_file_types_description
 		    @file_upload_limit      = Ckeditor.swf_file_upload_limit
 		  end
-      
+
       @swf_file_post_name ||= 'data'
       @file_size_limit ||= "5 MB"
       @file_types ||= "*.jpg;*.jpeg;*.png;*.gif"
